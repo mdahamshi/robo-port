62523bf365ff4abe4904aa0edc46a769e020f7a9
mdahamshi <mmd.0@hotmail.com>
2017-05-25 11:16:53 +0300

    some fixes

--- pushbullet/parser.py
+++ pushbullet/parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
-
+# -*- coding: utf-8 -*-
 import subprocess
 import os
 import sys
 import pwd  
 import signal
+import codecs
 global program
 global chars
 chars = set('\;\&\|')
@@ -15,6 +16,7 @@ env['HOME'] = pw_record.pw_dir
 env['LOGNAME'] = pw_record.pw_name
 env['USER'] = pw_record.pw_name
 env['USERNAME'] = pw_record.pw_name
+env['LANG'] = 'en_US.UTF-8'
 
 def parse(message):
     """This function parse a push message and run the corresponding command """
@@ -24,17 +26,22 @@ def parse(message):
         subProcess("pbme \"Attack detetcted !\"")
         return
     arguments = message.split('::')
+
+    if arguments[0] == 'speak' or arguments[0] == 'speakr':
+        speakHelper(arguments[1],arguments[0])
+        return
+    
     print "get args: ",arguments
     command = getCommand(arguments[0].lower()) 
     if command == "restart":
         subProcess('/usr/bin/play-atte')
         replaceMe("pushServer")
-        
-    arguments[0] =  "/usr/bin/" + command
+    
+    arguments[0] =   command
     print "new process args: ",arguments
     newProgram = subProcess(arguments)
 
-
+    
 def getCommand(message):
     return {
         'radio': "start-radio"
@@ -49,7 +56,7 @@ def getCommand(message):
         , 'up': "volume-up"
         , 'restart': "restart"
         , 'imhere': "play-arrive"
-        , 'speak': "robo-speak"
+        , 'speak': "echo -e "
     }.get(message, "pbme \"pushServer: wrong command received !\";echo "+message+" > /data/tmp/err")
 
 def getEnvo(message):
@@ -67,3 +74,13 @@ def subProcess(sub):
     subprocess.Popen(sub, shell=True \
     , preexec_fn=chuser(pw_record.pw_uid, pw_record.pw_gid),env=env)  
   
+def speakHelper(msg, repeat):
+    msgToSpeakFile = codecs.open('/data/tmp/msg.js','w','utf-8')
+    msgToSpeak = msg
+    if repeat == 'speakr':    #speakr = speak + repeat
+        msgToSpeak = msgToSpeak +unicode('، أُكَرِّر ،','utf-8')+ msg 
+    msgToSpeakFile.write('var message = '+ msgQuote(msgToSpeak))
+    subProcess('robo-speak')
+
+def msgQuote(msg):
+    return '\"' + msg + '\"'
